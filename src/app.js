54819f2c33c12af2fd94563f16f41717d9668e0a
shailu0007 <shailendradhakad314@gmail.com>
2026-01-11 17:20:52 +0530

    improve folder structure

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
@@ -8,4 +8,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-module.exports = app;
+export default app;
+
+
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-const app = require("./app");
+import app from './app.js';
+import {env} from './config/index.js'
 
-const PORT = process.env.PORT || 5000;
+const PORT = env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
