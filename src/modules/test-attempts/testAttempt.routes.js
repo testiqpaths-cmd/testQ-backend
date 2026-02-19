2ddf91d0375d327582cc5c170eed0cb5c44389b3
Sneha Gupta <snehasathi@gmail.com>
2026-02-19 12:47:45 +0530

    feat/SCRUM-24 Fetch Test Result

--- src/modules/test-attempts/testAttempt.routes.js
+++ src/modules/test-attempts/testAttempt.routes.js
@@ -3,7 +3,7 @@ import {authMiddleware} from '../../common/middlewares/auth.middleware.js';
 import { roleMiddleware } from "../../common/middlewares/role.middleware.js";
 import {startTestAttemptController , saveAnswerController, submitAttemptController,
   getAttemptController,
-  evaluateAttemptController,} from '../test-attempts/testAttempt.controller.js';
+  evaluateAttemptController,getAttemptResultController} from '../test-attempts/testAttempt.controller.js';
 import {loadAttempt  , enforceAttemptTimer} from '../test-attempts/middlewares/enforceAttemptTimer.middleware.js';
 import { validate } from '../../common/middlewares/validate.middleware.js';
 import { evaluateAttemptSchema } from './schemas/evaluateAttempt.schema.js';
@@ -23,4 +23,7 @@ router.post('/submitattempt/:attemptId', authMiddleware, loadAttempt, enforceAtt
 router.get('/getattempt/:attemptId', authMiddleware, loadAttempt, enforceAttemptTimer, getAttemptController);
 
 router.post('/manualevaluate/:attemptId',authMiddleware,roleMiddleware("IQPATH_ADMIN", "ORGANIZATION"),loadAttempt,validate(evaluateAttemptSchema),evaluateAttemptController);
+
+router.get('/getattemptresult/:attemptId', authMiddleware, getAttemptResultController);
+
 export default router;
