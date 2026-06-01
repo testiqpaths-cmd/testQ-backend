3d95d59ed2b16bbe67bea1bdf004199ba490858c
testiqpaths-cmd <test.iqpaths@gmail.com>
2026-06-01 22:25:15 +0530

    solve cors issue

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://test-q-frontend.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
